0d97d37b76a8c655ee944c261d7c75e308cac2ca
Pragmatic-Learner <nardevjuggoostudying@gmail.com>
2025-10-30 22:10:19 +0400

    Fourth commit

--- CW1PTP6.bat
+++ CW1PTP6.bat
@@ -1,36 +1,36 @@
 @echo off
 
-set main_folder="The_bag"
+set main_folder = The_bag
 
-set /p path="Provide a path : "
+set /p path = Provide a path : 
 
-if exist "%path%" (
-	if exist "%path%\%main_folder%" (
-		echo "ERROR!!  %path%\%main_folder% already exists!!"
+if exist %path% (
+	if exist %path%\%main_folder% (
+		echo ERROR!!  %path%\%main_folder% already exists!!
 		goto eof
 	)
-	cd "%path%"
+	cd %path%
 ) else (
-	echo "ERROR!!  Path does not exist, please enter new, exisiting path..."
+	echo ERROR!!  Path does not exist, please enter new, exisiting path...
 	goto eof
 )
 
-mkdir "%main_folder%"
-cd "%main_folder%"
+mkdir %main_folder%
+cd %main_folder%
 
 for /l %%x in (1,1,3) do (
-	mkdir "Folder%%x"
-	type nul > "Folder%%x\File%%x.txt"
-	if not exist "Folder%%x" (
-		echo "ERROR!!  Folder%%x was not created!!"
+	mkdir Folder%%x
+	type nul > Folder%%x\File%%x.txt
+	if not exist Folder%%x (
+		echo ERROR!!  Folder%%x was not created!!
 		goto eof
-	) else if not exist "Folder%%x\File%%x.txt" (
-		echo "ERROR!!  File%%x was not created!!"
+	) else if not exist Folder%%x\File%%x.txt (
+		echo ERROR!!  File%%x was not created!!
 		goto eof
 	)
 )
 
-echo “The 6 items are in The Bag!”
+echo The 6 items are in The Bag!
 
 :eof
 pause
--- CW1PTP8.bat
+++ CW1PTP8.bat
@@ -1,40 +1,40 @@
 @echo off
 
-set main_folder="myBatchScript"
-set txt_file="results.txt"
+set main_folder=myBatchScript
+set txt_file=results.txt
 
-set /p path="Enter path for creating files : "
+set /p path=Enter path for creating files : 
 
-if exist "%path%" (
-	if exist "%path%\%main_folder%" (
-		echo "ERROR!!  %path%\%main_folder% already exists!!"
+if exist %path% (
+	if exist %path%\%main_folder% (
+		echo ERROR!!  %path%\%main_folder% already exists!!
 		goto eof
 	)
-	cd "%path%"
+	cd %path%
 ) else (
-	echo "Path does not exist, please enter new, exisiting path..."
+	echo Path does not exist, please enter new, exisiting path...
 	goto eof
 )
 
-mkdir "%main_folder%"
-type nul > "%txt_file%"
-if not exist "%main_folder%" (
-	echo "ERROR!!  %main_folder% was not created!!"
+mkdir %main_folder%
+type nul > %txt_file%
+if not exist %main_folder% (
+	echo ERROR!!  %main_folder% was not created!!
 	goto eof
-) else if not exist "%main_folder%\%txt_file%" (
-	echo "ERROR!!  %txt_file% was not created!!"
+) else if not exist %main_folder%\%txt_file% (
+	echo ERROR!!  %txt_file% was not created!!
 	goto eof
 )
-echo "Knock, knock." > "%txt_file%"
-echo "(Who’s there?)" > "%txt_file%"
-echo "Recursion." > "%txt_file%"
-echo "(Recursion who?)" > "%txt_file%"
-echo "Knock Knock…" > "%txt_file%"
+echo Knock, knock. > %txt_file%
+echo (Who’s there?) > %txt_file%
+echo Recursion. > %txt_file%
+echo (Recursion who?) > %txt_file%
+echo Knock Knock… > %txt_file%
 
-copy "%txt_file%" "%main_folder%"
-del "%txt_file%"
+copy %txt_file% %main_folder%
+del %txt_file%
 
-echo "Task Complete!"
+echo Task Complete!
 
 :eof
 pause
