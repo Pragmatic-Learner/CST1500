e79d8d8d1b8ebbf373ca31aa34c1d596e3b3d5c4
Pragmatic-Learner <nardevjuggoostudying@gmail.com>
2025-10-30 22:28:48 +0400

    Fifth commit

--- CW1PTP6.bat
+++ CW1PTP6.bat
@@ -1,36 +1,35 @@
 @echo off
 
-set main_folder = The_bag
+set main_folder = "The_bag"
+set /p path = "Provide a path : "
 
-set /p path = Provide a path : 
-
-if exist %path% (
-	if exist %path%\%main_folder% (
-		echo ERROR!!  %path%\%main_folder% already exists!!
+if exist "%path%" (
+	if exist "%path%\%main_folder%" (
+		echo "ERROR!!  %path%\%main_folder% already exists!!"
 		goto eof
 	)
 	cd %path%
 ) else (
-	echo ERROR!!  Path does not exist, please enter new, exisiting path...
+	echo "ERROR!!  %path% does not exist, please enter new, exisiting path..."
 	goto eof
 )
 
-mkdir %main_folder%
-cd %main_folder%
+mkdir "%main_folder%"
+cd "%main_folder%"
 
 for /l %%x in (1,1,3) do (
-	mkdir Folder%%x
-	type nul > Folder%%x\File%%x.txt
-	if not exist Folder%%x (
-		echo ERROR!!  Folder%%x was not created!!
+	mkdir "Folder%%x"
+	type nul > "Folder%%x\File%%x.txt"
+	if not exist "Folder%%x" (
+		echo "ERROR!!  Folder%%x was not created!!"
 		goto eof
 	) else if not exist Folder%%x\File%%x.txt (
-		echo ERROR!!  File%%x was not created!!
+		echo "ERROR!!  File%%x was not created!!"
 		goto eof
 	)
 )
 
-echo The 6 items are in The Bag!
+echo "The 6 items are in The Bag!"
 
 :eof
 pause
